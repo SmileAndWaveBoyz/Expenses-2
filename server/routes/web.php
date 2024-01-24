f110693f5c4144d49b19fccfd1d7efcdec37befb
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2024-01-24 21:05:47 +0000

    Server user count

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $users = User::count();
+    return view('welcome', ["users" => $users]);
 });
 
 Route::get('/login', function () {

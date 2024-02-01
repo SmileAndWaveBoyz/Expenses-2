6dee6cd4b81e0ca10df99d678c748ce03440f3b9
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2024-02-01 09:47:11 +0000

    Set Filter input color.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -76,18 +76,18 @@ function Header() {
         <button onClick={filterButtonClick}  className='header__filterButton'><p className='header__filterPar'>Filter&nbsp;</p> <p className='header__filterPar mobileNoDisplay'> by status</p>  <svg style={{transform: `rotateX(${arrowRotate}deg)`}}  className='header__filterButtonSVG' xmlns="http://www.w3.org/2000/svg" width="10" height="7" viewBox="0 0 10 7" fill="none">  <path d="M1 6.22803L5.2279 2.00013L9.4558 6.22803" stroke="#7C5DFA" stroke-width="2"/></svg></button>
         <div className="inputContainer">
             <form className='inputContainer__form' style={{display: listDisplay}}>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="draft" onClick={checkBox}  defaultChecked />
-                <label class="form-check-label"> Draft</label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="pending" onClick={checkBox}  defaultChecked />
-                <label class="form-check-label"> Pending</label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="paid" onClick={checkBox}  defaultChecked />
-                <label class="form-check-label"> Paid </label>
-            </div>
+              <div class="form-check">
+                  <input class="form-check-input" type="checkbox" name="draft" onClick={checkBox}  defaultChecked />
+                  <label class="form-check-label"> Draft</label>
+              </div>
+              <div class="form-check">
+                  <input class="form-check-input" type="checkbox" name="pending" onClick={checkBox}  defaultChecked />
+                  <label class="form-check-label"> Pending</label>
+              </div>
+              <div class="form-check">
+                  <input class="form-check-input" type="checkbox" name="paid" onClick={checkBox}  defaultChecked />
+                  <label class="form-check-label"> Paid </label>
+              </div>
             </form>
         </div>
         <button onClick={newInvoice} className='btn btn-primary header__newButton'><svg className='plusSVG' xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none"><circle cx="16" cy="16" r="16" fill="white"/><path d="M17.3131 21.0234V17.3136H21.0229V14.7333H17.3131V11.0234H14.7328V14.7333H11.0229V17.3136H14.7328V21.0234H17.3131Z" fill="#7C5DFA"/></svg>New</button>
